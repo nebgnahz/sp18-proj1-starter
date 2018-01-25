c036a95b6b4f96bfe2c1b59e9e2af156b2ae2a0e
Ben Zhang <benzh@cs.berkeley.edu>
2018-01-24 18:05:21 -0800

    :books: update function documentations

--- philphix.c
+++ philphix.c
@@ -25,7 +25,12 @@ int main(int argc, char **argv){
   return 0;
 }
 
-/* Define a hash function */
+/*
+ * You need to implement this string hash function.
+ *
+ * void *s can be safely casted to a char * (null terminated string) which is
+ * done for you here for convenience.
+ */
 unsigned int stringHash(void *s){
   char *string = (void *) s;
   /* Printing string is to prevent a compiler warning until you
@@ -36,13 +41,28 @@ unsigned int stringHash(void *s){
   return 0;
 }
 
-
+/*
+ * You need to implement this function.
+ *
+ * It should return a nonzero value if the two strings are identical (case
+ * sensitive comparison) and 0 otherwise.
+ */
 int stringEquals(void *s1, void *s2){
   fprintf(stderr, "You need to implement stringEquals");
   /* To suppress compiler warning until you implement this function */
   return 0;
 }
 
+/*
+ * You need to implement this function.
+ *
+ * This function should read in every word in the dictionary and store it in the
+ * dictionary. You should first open the file specified, then read the content
+ * and insert them into the dictionary (use `insertData` function defined in
+ * `hashtable.h`). As described in the specs, you can initially assume that no
+ * word is longer than 60 characters.  However, for the final 30% of your grade,
+ * you cannot assumed that words have a bounded length.
+ */
 void readDictionary(char *name){
   FILE *inputFile = fopen(name, "r");
 
@@ -51,7 +71,14 @@ void readDictionary(char *name){
   fprintf(stderr, "You need to implement readDictionary %x\n", (unsigned int) inputFile);
 }
 
-
+/*
+ * You need to implement this function.
+ *
+ * The input will be provided as standard input (stdin), and you can print the
+ * processed results as standard output (stdout). You can print everything you
+ * want to standard error, it will be ignored by the autograder. You can use
+ * `findData` function defined in `hashtable.h`.
+ */
 void processInput(){
   fprintf(stderr, "You need to implement processInput\n");
 }
